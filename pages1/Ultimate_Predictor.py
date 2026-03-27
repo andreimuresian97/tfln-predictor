4c4b73bb1143919c0231f2da8b29969314854f7a
andreimuresian97 <andrei.muresian@fiber-resources.com>
2026-03-27 14:00:42 +0100

    Update Ultimate_Predictor.py

--- pages1/Ultimate_Predictor.py
+++ pages1/Ultimate_Predictor.py
@@ -335,8 +335,8 @@ if st.button("🚀 Predict Performance", type="primary"):
         
         # Hardcoded Global MAEs from your cross-validation
         mae_nm = 0.0264
-        mae_zc = 1.0     # <-- Put your actual Zc MAE here
-        mae_vpi = 0.045  # <-- Put your actual Vpi MAE here
+        mae_zc = 0.7726     # <-- Put your actual Zc MAE here
+        mae_vpi = 0.0130  # <-- Put your actual Vpi MAE here
         mae_alpha60 = 0.15 # <-- Put your actual Alpha 60 MAE here
         
         data = [
